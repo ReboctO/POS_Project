ec841d8bff8b1794ac7c3c0a340d4b8159492837
unknown <WW224@ccs.local>
2024-05-20 13:59:22 -0700

    first commit

---
+++ POS_Project/Views/Common/MainView/MainContainer.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace POS_Project.Views.Common.MainView
+{
+    partial class MainContainer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel3 = new Panel();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            iconButton2 = new FontAwesome.Sharp.IconButton();
+            iconButton3 = new FontAwesome.Sharp.IconButton();
+            iconButton4 = new FontAwesome.Sharp.IconButton();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = Color.FromArgb(119, 107, 93);
+            flowLayoutPanel1.Controls.Add(panel3);
+            flowLayoutPanel1.Controls.Add(iconButton1);
+            flowLayoutPanel1.Controls.Add(iconButton4);
+            flowLayoutPanel1.Controls.Add(iconButton3);
+            flowLayoutPanel1.Controls.Add(iconButton2);
+            flowLayoutPanel1.Dock = DockStyle.Left;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(173, 439);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            panel3.Location = new Point(3, 3);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(170, 78);
+            panel3.TabIndex = 2;
+            // 
+            // iconButton1
+            // 
+            iconButton1.FlatAppearance.BorderColor = Color.Gray;
+            iconButton1.FlatStyle = FlatStyle.Flat;
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.HomeLgAlt;
+            iconButton1.IconColor = Color.Black;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.IconSize = 30;
+            iconButton1.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton1.Location = new Point(3, 87);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(170, 36);
+            iconButton1.TabIndex = 0;
+            iconButton1.Text = "DashBoard";
+            iconButton1.UseVisualStyleBackColor = true;
+            // 
+            // iconButton2
+            // 
+            iconButton2.FlatAppearance.BorderColor = Color.Gray;
+            iconButton2.FlatStyle = FlatStyle.Flat;
+            iconButton2.IconChar = FontAwesome.Sharp.IconChar.HomeLgAlt;
+            iconButton2.IconColor = Color.Black;
+            iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton2.IconSize = 30;
+            iconButton2.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton2.Location = new Point(3, 213);
+            iconButton2.Name = "iconButton2";
+            iconButton2.Size = new Size(170, 36);
+            iconButton2.TabIndex = 1;
+            iconButton2.Text = "DashBoard";
+            iconButton2.UseVisualStyleBackColor = true;
+            // 
+            // iconButton3
+            // 
+            iconButton3.FlatAppearance.BorderColor = Color.Gray;
+            iconButton3.FlatStyle = FlatStyle.Flat;
+            iconButton3.IconChar = FontAwesome.Sharp.IconChar.ListCheck;
+            iconButton3.IconColor = Color.Black;
+            iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton3.IconSize = 30;
+            iconButton3.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton3.Location = new Point(3, 171);
+            iconButton3.Name = "iconButton3";
+            iconButton3.Size = new Size(170, 36);
+            iconButton3.TabIndex = 2;
+            iconButton3.Text = "DashBoard";
+            iconButton3.UseVisualStyleBackColor = true;
+            // 
+            // iconButton4
+            // 
+            iconButton4.FlatAppearance.BorderColor = Color.Gray;
+            iconButton4.FlatStyle = FlatStyle.Flat;
+            iconButton4.IconChar = FontAwesome.Sharp.IconChar.CartArrowDown;
+            iconButton4.IconColor = Color.Black;
+            iconButton4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton4.IconSize = 30;
+            iconButton4.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton4.Location = new Point(3, 129);
+            iconButton4.Name = "iconButton4";
+            iconButton4.Size = new Size(170, 36);
+            iconButton4.TabIndex = 3;
+            iconButton4.Text = "Orders";
+            iconButton4.UseVisualStyleBackColor = true;
+            // 
+            // MainContainer
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(flowLayoutPanel1);
+            Name = "MainContainer";
+            Size = new Size(622, 439);
+            Load += MainViewContainer_Load;
+            flowLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel panel3;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconButton iconButton4;
+        private FontAwesome.Sharp.IconButton iconButton3;
+        private FontAwesome.Sharp.IconButton iconButton2;
+    }
+}
